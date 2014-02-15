0ca438c8066e0ac294a6ea3e0db73b0a0c185e5a
MichaelRoeder <michaderstudent@hotmail.de>
2014-02-15 16:08:13 +0100

    Implemented Ratio Coherence.

---
+++ src/main/java/org/aksw/palmetto/calculations/RatioCoherenceCalculation.java
@@ -0,0 +1,33 @@
+package org.aksw.palmetto.calculations;
+
+import org.aksw.palmetto.subsets.SubsetCreator;
+import org.aksw.palmetto.subsets.SubsetProbabilities;
+
+public class RatioCoherenceCalculation extends AbstractSubsetCreatorBasedCoherenceCalculation {
+
+    public RatioCoherenceCalculation(SubsetCreator subsetCreator) {
+        super(subsetCreator);
+    }
+
+    @Override
+    protected double calculateCoherence(SubsetProbabilities subsetProbabilities) {
+        double segmentProbability, conditionProbability, jointProbability;
+        double coherence = 0;
+        int count = 0;
+        for (int i = 0; i < subsetProbabilities.segments.length; ++i) {
+            segmentProbability = subsetProbabilities.probabilities[subsetProbabilities.segments[i]];
+            if (segmentProbability > 0) {
+                for (int j = 0; j < subsetProbabilities.conditions[i].length; ++j) {
+                    conditionProbability = subsetProbabilities.probabilities[subsetProbabilities.conditions[i][j]];
+                    jointProbability = subsetProbabilities.probabilities[subsetProbabilities.segments[i]
+                            | subsetProbabilities.conditions[i][j]];
+                    if ((conditionProbability > 0) && (jointProbability > 0)) {
+                        coherence += jointProbability / (segmentProbability * conditionProbability);
+                    }
+                }
+            }
+            count += subsetProbabilities.conditions[i].length;
+        }
+        return coherence / count;
+    }
+}
