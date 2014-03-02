b3b1a9864905503367443788d1eac6a1f9d6669d
MichaelRoeder <michaderstudent@hotmail.de>
2014-03-02 19:34:10 +0100

    Implemented condition prob coherence, log condition prob coherence and Olssons calculation.

---
+++ src/main/java/org/aksw/palmetto/calculations/LogCondProbCoherenceCalculation.java
@@ -0,0 +1,41 @@
+package org.aksw.palmetto.calculations;
+
+import org.aksw.palmetto.subsets.SubsetProbabilities;
+
+public class LogCondProbCoherenceCalculation implements CoherenceCalculation {
+
+    @Override
+    public double[] calculateCoherenceValues(SubsetProbabilities subsetProbabilities) {
+        int pos = 0;
+        for (int i = 0; i < subsetProbabilities.segments.length; ++i) {
+            pos += subsetProbabilities.conditions[i].length;
+        }
+        double values[] = new double[pos];
+
+        double segmentProbability, conditionProbability, intersectionProbability;
+        pos = 0;
+        for (int i = 0; i < subsetProbabilities.segments.length; ++i) {
+            segmentProbability = subsetProbabilities.probabilities[subsetProbabilities.segments[i]];
+            if (segmentProbability > 0) {
+                for (int j = 0; j < subsetProbabilities.conditions[i].length; ++j) {
+                    conditionProbability = subsetProbabilities.probabilities[subsetProbabilities.conditions[i][j]];
+                    intersectionProbability = subsetProbabilities.probabilities[subsetProbabilities.segments[i]
+                            | subsetProbabilities.conditions[i][j]];
+                    if ((conditionProbability > 0)) {
+                        values[pos] = Math.log((intersectionProbability + LogBasedCalculation.EPSILON)
+                                / conditionProbability);
+                    }
+                    ++pos;
+                }
+            } else {
+                pos += subsetProbabilities.conditions[i].length;
+            }
+        }
+        return values;
+    }
+
+    @Override
+    public String getCalculationName() {
+        return "m_lc";
+    }
+}
---
+++ src/main/java/org/aksw/palmetto/calculations/OlssonsCalculation.java
@@ -0,0 +1,55 @@
+package org.aksw.palmetto.calculations;
+
+import org.aksw.palmetto.subsets.SubsetProbabilities;
+
+public class OlssonsCalculation implements CoherenceCalculation {
+
+    @Override
+    public double[] calculateCoherenceValues(SubsetProbabilities subsetProbabilities) {
+        int pos = 0;
+        for (int i = 0; i < subsetProbabilities.segments.length; ++i) {
+            pos += subsetProbabilities.conditions[i].length;
+        }
+        double values[] = new double[pos];
+
+        double intersectionProbability, jointProbability;
+        pos = 0;
+        for (int i = 0; i < subsetProbabilities.segments.length; ++i) {
+            for (int j = 0; j < subsetProbabilities.conditions[i].length; ++j) {
+                intersectionProbability = subsetProbabilities.probabilities[subsetProbabilities.segments[i]
+                        | subsetProbabilities.conditions[i][j]];
+                if (intersectionProbability > 0) {
+                    jointProbability = determineJointProbability(subsetProbabilities.segments[i]
+                            | subsetProbabilities.conditions[i][j], subsetProbabilities.probabilities);
+                    if (jointProbability > 0) {
+                        values[pos] = intersectionProbability / jointProbability;
+                    }
+                }
+                ++pos;
+            }
+        }
+        return values;
+    }
+
+    private double determineJointProbability(int jointBits, double[] probabilities) {
+        double jointProbability = 0;
+        int inverseMask = ~jointBits;
+        for (int i = 1; i <= jointBits; i++) {
+            // if all bits of the current i are elements of the joint set
+            if ((inverseMask & i) == 0) {
+                // if the number of elements are even
+                if ((Integer.bitCount(i) & 1) == 0) {
+                    jointProbability -= probabilities[i];
+                } else {
+                    jointProbability += probabilities[i];
+                }
+            }
+        }
+        return jointProbability;
+    }
+
+    @Override
+    public String getCalculationName() {
+        return "m_o";
+    }
+}
