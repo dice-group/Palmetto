0ea5a107dc5829e5f004b51c5c220a17b87fb331
MichaelRoeder <michaderstudent@hotmail.de>
2015-09-30 14:50:14 +0200

    Updated the licenses inside the single files.

--- src/main/java/org/aksw/palmetto/calculations/direct/ShogenjisConfirmationMeasure.java
+++ src/main/java/org/aksw/palmetto/calculations/direct/ShogenjisConfirmationMeasure.java
@@ -1,6 +1,19 @@
 /**
- * The MIT License
- * Copyright (c) 2014 Agile Knowledge Engineering and Semantic Web (AKSW)
+ * This file is part of Palmetto.
+ *
+ * Palmetto is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Palmetto is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Palmetto.  If not, see <http://www.gnu.org/licenses/>.
+ */
 							(roeder@informatik.uni-leipzig.de)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
