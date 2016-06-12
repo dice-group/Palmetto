37e2eae065568a45f44919da3b78b79f24d4dfda
mroeder <roeder@informatik.uni-leipzig.de>
2016-06-12 14:21:46 +0200

    Included Palmetto 1.1.0. Updated license information.

--- src/main/java/org/aksw/palmetto/webapp/config/RootConfig.java
+++ src/main/java/org/aksw/palmetto/webapp/config/RootConfig.java
@@ -1,4 +1,20 @@
 /**
+ * This file is part of Palmetto Web Application.
+ *
+ * Palmetto Web Application is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Palmetto Web Application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Palmetto Web Application.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
  * This file is part of General Entity Annotator Benchmark.
  *
  * General Entity Annotator Benchmark is free software: you can redistribute it and/or modify
