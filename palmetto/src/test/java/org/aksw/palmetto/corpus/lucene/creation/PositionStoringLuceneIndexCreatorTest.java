9b6911a791e69c2bab6ad235c9ae23c7afc98589
MichaelRoeder <michaasresearcher@gmail.com>
2016-11-09 19:11:25 +0100

    Removed a test class that was based on an old Lucene index. Integrated the test case into an already existing test class.

--- palmetto/src/test/java/org/aksw/palmetto/corpus/lucene/creation/PositionStoringLuceneIndexCreatorTest.java
+++ palmetto/src/test/java/org/aksw/palmetto/corpus/lucene/creation/PositionStoringLuceneIndexCreatorTest.java
@@ -23,6 +23,10 @@ import java.util.Iterator;
 
 import org.aksw.palmetto.Palmetto;
 import org.aksw.palmetto.corpus.lucene.WindowSupportingLuceneCorpusAdapter;
+import org.aksw.palmetto.data.CountedSubsets;
+import org.aksw.palmetto.data.SegmentationDefinition;
+import org.aksw.palmetto.prob.window.BooleanSlidingWindowFrequencyDeterminer;
+import org.apache.commons.io.FileUtils;
 import org.apache.lucene.index.CorruptIndexException;
 import org.junit.Assert;
 import org.junit.Test;
@@ -36,15 +40,22 @@ public class PositionStoringLuceneIndexCreatorTest {
     public static IndexableDocument DOCUMENTS[] = { new IndexableDocument("This is a test document.", 5),
             new IndexableDocument("This is another test document.", 5),
             new IndexableDocument("This is the third.", 4) };
-    public static int WINDOW_SIZE = 3;
+    public static int WINDOW_SIZE = 4;
     public static String TEST_WORDS[] = { "is", "document", "dog" };
-    // expected positions of the words inside the documents (negativ number means that the word is not inside the
+    // expected positions of the words inside the documents (negativ number
+    // means that the word is not inside the
     // document
     public static int EXPECTED_WORD_POSITIONS[][] = new int[][] { { 1, 4, -1 }, { 1, 4, -1 }, { 1, -1, -1 } };
+    // counts for windows of size 4 containing { ALWAYS 0, "is", "document",
+    // "is"+"document", "dog", "is"+"dog", "document"+"dog",
+    // "is"+"document"+"dog"}
+    public static int EXPECTED_COUNTS[] = new int[] { 0, 5, 2, 2, 0, 0, 0, 0 };
 
     @Test
     public void test() throws CorruptIndexException, IOException {
-        File indexDir = createTempDirectory();
+        File indexDir = new File(
+                FileUtils.getTempDirectoryPath() + File.separator + "temp_index" + Long.toString(System.nanoTime()));
+        Assert.assertTrue(indexDir.mkdir());
         Iterator<IndexableDocument> docIterator = Arrays.asList(DOCUMENTS).iterator();
         // create the index
         PositionStoringLuceneIndexCreator creator = new PositionStoringLuceneIndexCreator(
@@ -56,46 +67,45 @@ public class PositionStoringLuceneIndexCreatorTest {
 
         // test the created index
         // create an adapter
-        WindowSupportingLuceneCorpusAdapter adapter = WindowSupportingLuceneCorpusAdapter.create(
-                indexDir.getAbsolutePath(), Palmetto.DEFAULT_TEXT_INDEX_FIELD_NAME,
-                Palmetto.DEFAULT_DOCUMENT_LENGTH_INDEX_FIELD_NAME);
-        // query the test words
-        IntIntOpenHashMap docLengths = new IntIntOpenHashMap();
-        IntObjectOpenHashMap<IntArrayList[]> wordPositions = adapter.requestWordPositionsInDocuments(TEST_WORDS,
-                docLengths);
-        // compare the result with the expected counts
-        int positionInDoc;
-        IntArrayList[] positionsInDocs;
-        for (int i = 0; i < EXPECTED_WORD_POSITIONS.length; ++i) {
-            positionsInDocs = wordPositions.get(i);
-            for (int j = 0; j < positionsInDocs.length; ++j) {
-                if (EXPECTED_WORD_POSITIONS[i][j] < 0) {
-                    Assert.assertNull("Expected null because the word \""
-                            + TEST_WORDS[j] + "\" shouldn't be found inside document " + i
-                            + ". But got a position list instead.",
-                            positionsInDocs[j]);
-                } else {
-                    Assert.assertEquals(1, positionsInDocs[j].elementsCount);
-                    positionInDoc = positionsInDocs[j].buffer[0];
-                    Assert.assertEquals("Expected the word \""
-                            + TEST_WORDS[j] + "\" in document " + i + " at position " + EXPECTED_WORD_POSITIONS[i][j]
-                            + " but got position " + positionInDoc + " form the index.",
-                            EXPECTED_WORD_POSITIONS[i][j], positionInDoc);
+        WindowSupportingLuceneCorpusAdapter adapter = null;
+        try {
+            adapter = WindowSupportingLuceneCorpusAdapter.create(indexDir.getAbsolutePath(),
+                    Palmetto.DEFAULT_TEXT_INDEX_FIELD_NAME, Palmetto.DEFAULT_DOCUMENT_LENGTH_INDEX_FIELD_NAME);
+            // query the test words
+            IntIntOpenHashMap docLengths = new IntIntOpenHashMap();
+            IntObjectOpenHashMap<IntArrayList[]> wordPositions = adapter.requestWordPositionsInDocuments(TEST_WORDS,
+                    docLengths);
+            // compare the result with the expected counts
+            int positionInDoc;
+            IntArrayList[] positionsInDocs;
+            for (int i = 0; i < EXPECTED_WORD_POSITIONS.length; ++i) {
+                positionsInDocs = wordPositions.get(i);
+                for (int j = 0; j < positionsInDocs.length; ++j) {
+                    if (EXPECTED_WORD_POSITIONS[i][j] < 0) {
+                        Assert.assertNull("Expected null because the word \"" + TEST_WORDS[j]
+                                + "\" shouldn't be found inside document " + i + ". But got a position list instead.",
+                                positionsInDocs[j]);
+                    } else {
+                        Assert.assertEquals(1, positionsInDocs[j].elementsCount);
+                        positionInDoc = positionsInDocs[j].buffer[0];
+                        Assert.assertEquals("Expected the word \"" + TEST_WORDS[j] + "\" in document " + i
+                                + " at position " + EXPECTED_WORD_POSITIONS[i][j] + " but got position " + positionInDoc
+                                + " form the index.", EXPECTED_WORD_POSITIONS[i][j], positionInDoc);
+                    }
                 }
             }
-        }
-    }
 
-    public static File createTempDirectory()
-            throws IOException {
-        File temp = File.createTempFile("temp_index", Long.toString(System.nanoTime()));
-        if (!(temp.delete())) {
-            return null;
+            // test the window based counting
+            BooleanSlidingWindowFrequencyDeterminer determiner = new BooleanSlidingWindowFrequencyDeterminer(adapter,
+                    WINDOW_SIZE);
+            CountedSubsets subsets = determiner.determineCounts(new String[][] { TEST_WORDS },
+                    new SegmentationDefinition[] { new SegmentationDefinition(new int[0], new int[0][0], null) })[0];
+            Assert.assertArrayEquals(EXPECTED_COUNTS, subsets.counts);
+        } finally {
+            if (adapter != null) {
+                adapter.close();
+            }
         }
-        if (!(temp.mkdir())) {
-            return null;
-        }
-        return temp;
     }
 
 }
