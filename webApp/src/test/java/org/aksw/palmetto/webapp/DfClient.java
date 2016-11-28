6fa73b8d753bf7fcf7d9db0ebd17c5802684ac10
MichaelRoeder <michaasresearcher@gmail.com>
2016-11-28 19:16:21 +0100

    Added a simple main method which calls the remote service instead of executing a local JUnit test. (refs #10)

--- webApp/src/test/java/org/aksw/palmetto/webapp/DfClient.java
+++ webApp/src/test/java/org/aksw/palmetto/webapp/DfClient.java
@@ -1,7 +1,13 @@
 package org.aksw.palmetto.webapp;
 
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.ByteBuffer;
 
+import org.apache.commons.io.Charsets;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -12,7 +18,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 // Ignored until there is a good way to use the index for tests in the CI
-//@Ignore
+@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = org.aksw.palmetto.webapp.config.RootConfig.class)
 public class DfClient {
@@ -45,9 +51,8 @@ public class DfClient {
 
     @Test
     public void testUnderscore() throws Exception {
-        ResponseEntity<byte[]> response = controller.requestDocFreq("dog dog_ cat");
+        ResponseEntity<byte[]> response = controller.requestDocFreq("dog foundation_year cat");
         ByteBuffer buffer = ByteBuffer.wrap(response.getBody());
-
 
         int length, docId;
         length = buffer.getInt();
@@ -60,7 +65,7 @@ public class DfClient {
         // dog_ should create an empty result
         length = buffer.getInt();
         Assert.assertEquals(0, length);
-        
+
         length = buffer.getInt();
         Assert.assertTrue(length > 0);
         Assert.assertTrue(length <= buffer.remaining());
@@ -81,4 +86,37 @@ public class DfClient {
         Assert.assertEquals(0, buffer.remaining());
     }
 
+    public static void main(String[] args) throws Exception {
+        String url = "http://palmetto.aksw.org/palmetto-webapp/service/df";
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // Send post request
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+        OutputStream out = con.getOutputStream();
+        out.write("words=cat foundation_year dog".getBytes(Charsets.UTF_8));
+        out.flush();
+        out.close();
+
+        System.out.println("Sending 'POST' request to URL : " + url);
+        System.out.println("Response Code : " + con.getResponseCode());
+
+        InputStream is = new BufferedInputStream(con.getInputStream());
+        ByteBuffer buffer;
+        byte bytes[];
+        int length;
+        while (is.available() > 0) {
+            bytes = new byte[4];
+            is.read(bytes);
+            buffer = ByteBuffer.wrap(bytes);
+            length = buffer.getInt();
+            System.out.println(length);
+            if (length > 0) {
+                buffer = ByteBuffer.allocate(length * 4);
+                is.read(buffer.array());
+            }
+        }
+        is.close();
+    }
 }
