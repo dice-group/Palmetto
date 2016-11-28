369b90a41d01994b53bb6c902d56b1e3c0d8af59
MichaelRoeder <michaasresearcher@gmail.com>
2016-11-28 18:59:14 +0100

    Adapted the JUnit test to the description of the problem described in #10.

--- webApp/src/test/java/org/aksw/palmetto/webapp/DfClient.java
+++ webApp/src/test/java/org/aksw/palmetto/webapp/DfClient.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 // Ignored until there is a good way to use the index for tests in the CI
-@Ignore
+//@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = org.aksw.palmetto.webapp.config.RootConfig.class)
 public class DfClient {
@@ -45,11 +45,29 @@ public class DfClient {
 
     @Test
     public void testUnderscore() throws Exception {
-        ResponseEntity<byte[]> response = controller.requestDocFreq("dog_");
+        ResponseEntity<byte[]> response = controller.requestDocFreq("dog dog_ cat");
         ByteBuffer buffer = ByteBuffer.wrap(response.getBody());
 
-        int length = buffer.getInt();
+
+        int length, docId;
+        length = buffer.getInt();
+        Assert.assertTrue(length > 0);
+        Assert.assertTrue(length <= buffer.remaining());
+        for (int i = 0; i < length; ++i) {
+            docId = buffer.getInt();
+            Assert.assertTrue(docId >= 0);
+        }
+        // dog_ should create an empty result
+        length = buffer.getInt();
         Assert.assertEquals(0, length);
+        
+        length = buffer.getInt();
+        Assert.assertTrue(length > 0);
+        Assert.assertTrue(length <= buffer.remaining());
+        for (int i = 0; i < length; ++i) {
+            docId = buffer.getInt();
+            Assert.assertTrue(docId >= 0);
+        }
         Assert.assertEquals(0, buffer.remaining());
     }
 
