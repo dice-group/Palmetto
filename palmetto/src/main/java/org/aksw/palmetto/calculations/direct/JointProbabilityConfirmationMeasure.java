a5a47de0bb1387556194412e0bf0d06bfc8ec668
Michael Röder <michael.roeder@uni-paderborn.de>
2022-09-19 15:02:40 +0200

    Added the simple joint probability as direct measure.

---
+++ palmetto/src/main/java/org/aksw/palmetto/calculations/direct/JointProbabilityConfirmationMeasure.java
@@ -0,0 +1,57 @@
+/**
+ * Palmetto - Palmetto is a quality measure tool for topics.
+ * Copyright © 2014 Data Science Group (DICE) (michael.roeder@uni-paderborn.de)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.aksw.palmetto.calculations.direct;
+
+import org.aksw.palmetto.data.SubsetProbabilities;
+
+/**
+ * This confirmation measure simply returns the joint probability of W' and W*
+ * as value. result = P(W',W*)
+ * 
+ * @author Michael Röder
+ * 
+ */
+public class JointProbabilityConfirmationMeasure implements DirectConfirmationMeasure {
+
+    public JointProbabilityConfirmationMeasure() {
+        super();
+    }
+
+    @Override
+    public double[] calculateConfirmationValues(SubsetProbabilities subsetProbabilities) {
+        int pos = 0;
+        for (int i = 0; i < subsetProbabilities.segments.length; ++i) {
+            pos += subsetProbabilities.conditions[i].length;
+        }
+        double values[] = new double[pos];
+        pos = 0;
+        for (int i = 0; i < subsetProbabilities.segments.length; ++i) {
+            for (int j = 0; j < subsetProbabilities.conditions[i].length; ++j) {
+                values[pos] = subsetProbabilities.probabilities[subsetProbabilities.segments[i]
+                        | subsetProbabilities.conditions[i][j]];
+                ++pos;
+            }
+        }
+        return values;
+    }
+
+    @Override
+    public String getName() {
+        return "m_P";
+    }
+}
